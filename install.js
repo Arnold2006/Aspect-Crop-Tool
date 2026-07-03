d44c0062044c352e4accc72eff01208e9bdae5a2
Arnold2006 <115367701+Arnold2006@users.noreply.github.com>
2026-07-03 19:03:34 +0200

    Update install.js

--- install.js
+++ install.js
@@ -1,1 +1,9 @@
 
+module.exports = async (p) => {
+  console.log("Installing requirements...");
+  
+  // Install python dependencies via pip
+  await p.install('requirements.txt', { pip: true });
+  
+  console.log("Installation complete!");
+};
